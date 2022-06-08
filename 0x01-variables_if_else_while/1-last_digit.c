d6b84114e893df403dcf5206eae6b89056fcad73
Armfath <info.armanagonou@gmail.com>
2022-06-08 00:22:20 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ int main(void)
 	printf("Last digit of %d is ", n);
 	if (lastDigit > 5)
 		printf("%d and is greater than 5\n", lastDigit);
-	else is (lastDigit == 0)
+	else if (lastDigit == 0)
 		printf("%d and is 0\n", lastDigit);
 	else
-		printf("%d and is less than 6 and not 0", lastDigit);
+		printf("%d and is less than 6 and not 0\n", lastDigit);
 
 	return (0);
 }
