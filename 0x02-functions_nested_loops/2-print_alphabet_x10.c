2c16393f66e4c64e1380905d3623513ae4c15c89
Armfath <info.armanagonou@gmail.com>
2022-06-08 12:05:57 -0700

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,17 @@
  *
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+    for (i = 0; i < 10; i++)
+    {
+        for (i = 97; i <= 122; i++)
 		{
 		_putchar(i);
 		}
 		_putchar('\n');
+    }
+
 }
