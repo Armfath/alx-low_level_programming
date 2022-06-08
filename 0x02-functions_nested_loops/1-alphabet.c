1684aeaca02b1b3e07745a9e6baf795f460d6748
Armfath <info.armanagonou@gmail.com>
2022-06-08 11:56:55 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase
+ * print_alphabet - prints the alphabet, in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
-int main(void)
+void print_alphabet(void)
 {
 	int i;
 
@@ -14,6 +14,4 @@ int main(void)
 		_putchar(i);
 		}
 		_putchar('\n');
-
-	return (0);
 }
