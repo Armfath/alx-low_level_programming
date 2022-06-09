99d3ae587768a99b1347ea49796e18db228f7680
Armfath <info.armanagonou@gmail.com>
2022-06-08 23:06:35 -0700

    prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, m;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (i=0 ; i < 10 ; i++)
+		{
+			m = i * j;
+			if (m > 9)
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			else
+				_putchar(m);
+			if (i < 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
