2aef5c2a1aaff023a6602709afde0994f1400f19
Armfath <info.armanagonou@gmail.com>
2022-06-08 21:44:50 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,14 @@ int print_last_digit(int n)
 	int l;
 	l = n % 10;
 
-	_putchar((l % 10) + '0');
-	return (l);
+	if (l >= 0)
+	{
+		_putchar((l % 10) + '0');
+		return (l);
+	}
+	else
+	{
+		_putchar(((-1 * l) % 10) + '0');
+		return (-1 * l);
+	}
 }
