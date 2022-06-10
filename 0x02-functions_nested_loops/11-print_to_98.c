45421c16de17afebd31ee9b48e4ea60d57076d69
Armfath <info.armanagonou@gmail.com>
2022-06-10 07:41:17 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,9 @@
 
 void print_to_98(int n)
 {
-	for (; n <= 98 ; n++)
+	for (; n < 98 ; n++)
 	{
-		printf("%d", n);
-		if (n < 98)
-			printf(", ");
+		printf("%d, ", n);
 	}
 	_putchar('\n');
 }
