c11689ee98030e96f5992565eb5aeb8dfea98897
Armfath <info.armanagonou@gmail.com>
2022-06-12 00:00:34 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0 ; j < 9 ; j++)
+	for (j = 0 ; j <= 9 ; j++)
 	{
 		for (i = 0 ; i <= 14 ; i++)
 		{
 			if (i > 9)
 			{
-				_putchar((1 % 10) + '0');
+				_putchar(49);
 			}
 			_putchar((i % 10) + '0');
 		}
