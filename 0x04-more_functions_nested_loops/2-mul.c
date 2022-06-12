bf53d4aa5019d87106cd7396dae1601650a70522
Armfath <info.armanagonou@gmail.com>
2022-06-11 22:56:58 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplie two integers
+ *
+ * @a: integer_1
+ * @b: integer_2
+ *
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
