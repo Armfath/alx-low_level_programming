4bde1d44172c8aebb121b6761a8ba083f3e1321c
Armfath <info.armanagonou@gmail.com>
2022-06-11 23:28:34 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 14 ; i++)
+	{
+		if (i < 10)
+		_putchar((i % 10) + '0');
+		else
+		{
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
