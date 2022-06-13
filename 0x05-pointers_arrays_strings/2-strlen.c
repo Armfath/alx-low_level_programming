d95b2471a30824707b957f9d7bf45c6ef6c23d96
Armfath <info.armanagonou@gmail.com>
2022-06-13 15:09:59 -0700

    the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - count the length of a string
+ *
+ * @s: the string
+ *
+ * Return: the length os s
+ */
+
+int _strlen(char *s)
+{
+	int i, j;
+
+	j=0;
+	for (i = 0 ; s[i] == '\0' ; i++)
+		j++;
+}
