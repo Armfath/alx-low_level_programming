32a768813bc5805b5ca56edcff1ce9b1c0758da7
Armfath <info.armanagonou@gmail.com>
2022-06-13 19:59:21 -0700

    jjj

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,20 +8,16 @@
  * Return: void
  */
 
-void print_rev(char *s)
 {
-	int i;
+		int len;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		_putchar(s[i]);
-		i--;
-	}
-	_putchar('\n');
+			for (len = 0; s[len] != '\0'; len++)
+					{
+							}
+				for (len = len - 1; len >= 0; len--)
+						{
+									_putchar(s[len]);
+										}
+					_putchar('\n');
 }
+
