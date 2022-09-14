31fa1adfea4921a48da93d4fdc577cc712713b03
Armfath <info.armanagonou@gmail.com>
2022-09-13 19:50:58 -0700

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet, in lowercase
+ * print_alphabet_x10 - prints the alphabet, in lowercase 10 times
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-    int j;
+	int j;
 
-    j = 0;
-    while (j < 10)
-    {
-        int i;
+	j = 0;
+	while (j < 10)
+	{
+		int i;
 
-        for (i = 97; i < 123; i++)
-            _putchar(i);
-        _putchar('\n');
-        j++;
-    }
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+		_putchar('\n');
+		j++;
+	}
 }
