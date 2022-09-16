96524bb9e1b9345252fe1f38af5fa399c47e6ce1
Armfath <info.armanagonou@gmail.com>
2022-09-16 03:12:03 -0700

    if uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
+* _isupper - checks for uppercase character
+* @c: month in number format
+* Return: 1 if c is uppercase / 0 otherwise
+*/
 
-    c = 'o';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
