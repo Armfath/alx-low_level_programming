6f46a99fcd7720dc0dd8a7378d7855221401295d
Armfath <info.armanagonou@gmail.com>
2022-09-30 09:32:06 -0700

    number of passed arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+ * main - print the number of arguments
+ * @argc: number of parameter passed
+ * @argv: array containing the parameters
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
