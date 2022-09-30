af98561d6be528c4c49401b80f731f505f2dc24e
Armfath <info.armanagonou@gmail.com>
2022-09-30 09:03:14 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - print the programm name
+ * @argc: number of parameter passed
+ * @argv: array containing the parameters
+ * Return: Always 0
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv + argc);
+	return (0);
+}
