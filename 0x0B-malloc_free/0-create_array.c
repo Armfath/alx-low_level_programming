6e93eecd632b5599a27e270444cdbc5763ba5c2e
Armfath <info.armanagonou@gmail.com>
2022-10-04 05:24:46 -0700

    create arry with given size malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array
+ * @c: the initial char
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *str;
+
+		str = malloc(sizeof(char) * size);
+		while (size + 1)
+		{
+			str[size] = c;
+			size--;
+		}
+		return (str);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+dynamic allocation and memory free
