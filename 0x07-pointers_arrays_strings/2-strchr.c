f0d5fa9ce29534c8803e32386d31abebf430d58a
Armfath <info.armanagonou@gmail.com>
2022-10-09 15:25:16 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <string.h>
+/**
+ * _strchr - locates a character in a string.
+ * @s: the string
+ * @c: the char
+ *
+ * Return: the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	int len, i;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
