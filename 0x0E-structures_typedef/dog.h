17d0929959d4043b375c5fc744a5195dd46dc6b6
Armfath <info.armanagonou@gmail.com>
2022-10-11 03:58:23 -0700

    first strct created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Record a dog infos
+ * @name: the dod's name
+ * @age: the dod's age
+ * @owner: the dod's owner
+ */
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+
+#endif
