172806d06dc823a92f370082938953f9d9fc423b
Armfath <info.armanagonou@gmail.com>
2022-10-11 09:11:18 -0700

    00-1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,12 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct  dog));
-	if (!d)
+	if (d)
 	{
-		return;
+		d->name = name;
+        d->age = age;
+        d->owner = owner;
 	}
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	
 }
