820e8f00a9e16239bf3e9b7c41d210e64a7d668f
Armfath <info.armanagonou@gmail.com>
2022-10-11 08:30:35 -0700

    func that init a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a dog infos
+ * @d: the dog
+ * @name: the dod's name
+ * @age: the dod's age
+ * @owner: the dod's owner
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct  dog));
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
