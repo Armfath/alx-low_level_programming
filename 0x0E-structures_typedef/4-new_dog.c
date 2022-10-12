8bae3b2acbc5bb6fce0ae00d626d6ab8682edde3
Armfath <info.armanagonou@gmail.com>
2022-10-11 19:42:56 -0700

    4-00

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
  * new_dog - create a dog infos
@@ -11,15 +12,39 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+	int len_name = 0, len_owner = 0, i;
 
+	len_name = strlen(name) + 1;
+	len_owner = strlen(owner) + 1;
 	n_dog = malloc(sizeof(dog_t));
 	if (!n_dog)
 	{
 		free(n_dog);
 		return (NULL);
 	}
-	n_dog->name = name;
+	n_dog->name = malloc(len_name * sizeof(n_dog->name));
+	if (!n_dog->name)
+	{
+		free(n_dog->name);
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->owner = malloc(len_owner * sizeof(n_dog->owner));
+	if (!n_dog->owner)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	for (i = 0; i < len_name; i++)
+	{
+		n_dog->name[i] = name[i];
+	}
 	n_dog->age = age;
-	n_dog->owner = owner;
+	for (i = 0; i < len_owner; i++)
+	{
+		n_dog->owner[i] = owner[i];
+	}
 	return (n_dog);
 }
