7a7469a959ac8cff7984c746131cdfed472c5856
Armfath <info.armanagonou@gmail.com>
2022-10-19 02:44:26 -0700

    print variadic int

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers -  print its all given int
+ * @n: num of int
+ * @separator: int separator
+ *
+ * Return: sum of all of them
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg_p;
+	unsigned int i;
+	int num;
+
+	va_start(arg_p, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(arg_p, int));
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
+	}
+	va_end(arg_p);
+	putchar('\n');
+}
