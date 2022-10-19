d48761a247090feefe06017fcb890ab14ac13118
Armfath <info.armanagonou@gmail.com>
2022-10-19 03:00:13 -0700

    fix t-2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,15 +10,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arg_p;
 	unsigned int i;
+	char *str;
 
 	va_start(arg_p, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(arg_p, char *));
+		str = va_arg(arg_p, char *);
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
 		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
-		else if (separator == NULL && i < (n - 1))
-			printf("(nil)");
 	}
 	va_end(arg_p);
 	putchar('\n');
