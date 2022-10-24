461d751264341d1745e14d6f1333c2dec4538c5e
Armfath <info.armanagonou@gmail.com>
2022-10-24 14:15:23 -0700

    test orial's code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,24 +1,27 @@
 #include "lists.h"
+#include <string.h>
+
 /**
- * add_node - adds a new node at the beginning of linked list
- * @head: head of the l_l
- * @str: data (str)
- *
- * Return: head of the nex l_l
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to string that adds as node
+ * Return: pointer to new head of list, NULL on failure
+ * stephane = length
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	char *s;
+        list_t *temp;
+        int stephane = 0;
 
-	s = strdup(str);
-	new = malloc(sizeof(list_t));
-	if (new != NULL)
-	{
-		new->str = s;
-		new->len = strlen(s);
-		new->next = *head;
-		*head = new;
-	}
-	return (*head);
-}
+        temp = malloc(sizeof(list_t));
+        if (temp == NULL)
+                return (NULL);
+
+        while (str[stephane])
+                stephane++;
+
+        temp->len = stephane;
+        temp->str = strdup(str);
+        temp->next = *head;
+        *head = temp;
+        return (temp);
