b9385dd5faaa62c24e020389035a72d0b18bacad
Armfath <info.armanangou@gmail.com>
2022-10-25 16:00:57 +0100

    fix 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,14 @@
 #include "lists.h"
-
+/**
+ * listint_len - print int from listst
+ * @h: head of the list
+ *
+ * Return: num of the node in the list
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t i;
-	
+
 	i = 0;
 	while (h->next != NULL)
 	{
