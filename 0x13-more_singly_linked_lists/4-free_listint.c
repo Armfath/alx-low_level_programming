422cc6d0131d00a50254b39c29f51dfe43260cbe
Armfath <info.armanangou@gmail.com>
2022-10-25 17:05:06 +0100

    betty 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,18 @@
 #include "lists.h"
-
+/**
+ * free_listint - free l_l
+ * @head: list head
+ *
+ * Return: nothing
+ */
 void free_listint(listint_t *head)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    while (head != NULL)
-    {
-        tmp = head;
-        head = head->next;
-        free(tmp);
-    }
-    
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
