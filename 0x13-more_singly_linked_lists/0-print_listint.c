9e0d6514ed6ae1e6551724d77c228713b6ad8ea5
Armfath <info.armanangou@gmail.com>
2022-10-25 17:24:27 +0100

    try 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	i = 0;
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
+	
 	return (i);
 }
