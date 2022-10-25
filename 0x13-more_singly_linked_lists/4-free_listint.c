446e1f48b27519a233b3e6802bf55063539d49bf
Armfath <info.armanangou@gmail.com>
2022-10-25 13:37:21 +0100

    free l_l

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,10 +2,13 @@
 
 void free_listint(listint_t *head)
 {
+    listint_t *tmp;
+
     while (head != NULL)
     {
-        free(head);
+        tmp = head;
         head = head->next;
+        free(tmp);
     }
     
 }
