859407fcab9908d0885a8f35fcd2d41fbcce2904
Armfath <info.armanangou@gmail.com>
2022-10-25 16:46:22 +0100

    222

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,12 +11,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (new != NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-		new->next = *head;
-		*head = new;
-	}
+
+	new->n = n;
+	new->next = NULL;
+	new->next = *head;
+	*head = new;
+
 	return (*head);
 }
