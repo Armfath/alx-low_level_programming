429d01f1dfe7731e83cdb346d169012f6c56196c
Armfath <info.armanangou@gmail.com>
2022-10-25 17:15:31 +0100

    betty 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,22 @@
 #include "lists.h"
-
+/**
+ * free_listint2 - free whole list and set l_l to nil
+ * @head: list head
+ *
+ * Return: Nothing
+ */
 void free_listint2(listint_t **head)
 {
-    listint_t *tmp;
-    listint_t *h;
+	listint_t *tmp;
+	listint_t *h;
 
-    h = *head;
-    while (h->next != NULL)
-    {
-        tmp = h;
-        h = h->next;
-        free(tmp);
-    }
-    free(*head);
-    *head = NULL;
+	h = *head;
+	while (h->next != NULL)
+	{
+		tmp = h;
+		h = h->next;
+		free(tmp);
+	}
+	free(h);
+	*head = NULL;
 }
