bf91ef68288b6e16780c49cce22bec2a038af0bd
Armfath <info.armanangou@gmail.com>
2022-10-25 13:04:07 +0100

    first test

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *h;
+
+	h = *head;
+	new = malloc(sizeof(listint_t));
+	if (new != NULL)
+	{
+		new->n = n;
+		new->next = NULL;
+		if (h == NULL)
+		{
+			*head = new;
+			return (h);
+		}
+		while (h->next != NULL)
+		{
+			h = h->next;
+		}
+		h->next = new;
+		return (h);
+	}
+	else
+		return (NULL);
+}
