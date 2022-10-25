d89dd4a8e13d0a396512e4acc866aacea50ec15b
Armfath <info.armanangou@gmail.com>
2022-10-25 16:31:11 +0100

    2-betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * add_nodeint - ad node at begining
+ * @head: list head
+ * @n: node data
+ *
+ * Return: num of node in the list
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
