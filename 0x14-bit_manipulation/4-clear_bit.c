4c0b164cbacd5cf1b69092dd6694d36a772a07b8
Armfath <info.armanangou@gmail.com>
2022-10-28 17:02:28 +0100

    test 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int set = 1;
 
-	set <<= index;
-	if (~set == 0)
+	set = ~(set << index);
+	if (set == 0)
 		return (-1);
-	*n = (*n & ~set);
+	*n = (*n & set);
 
 	return (1);
 }
