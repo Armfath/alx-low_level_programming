33b0ce6f2e7e7b4b4c7c5a57db686d0e870770bb
ANAGONOU Armand <info.armanagonou.com>
2022-10-29 18:59:30 +0100

    fix strrspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,36 @@
-#include <string.h>
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: given string
+ * @accept: use for search
+ *
+ * Return: length of correspond sub-string
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	long unsigned int i, j;
 	unsigned int len = 0;
-	
-	for (i = 0; i < strlen(accept); i++)
+	int accept_len, s_len, exist = 0, i, j;
+
+	s_len = strlen(s);
+	accept_len = strlen(accept);
+	for (i = 0; i < s_len; i++)
 	{
-		for (j = 0; j < strlen(s); j++)
+		for (j = 0; j < accept_len; j++)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
-				len++;
+				printf("s[%d] = %c || a[%d] = %c\n", i, s[i], j, accept[j]);
+				exist = 1;
 				break;
 			}
+			else
+				exist = 0;
 		}
+		if (exist == 1)
+			len++;
+		else
+			return (len);
 	}
 	return (len);
 }
