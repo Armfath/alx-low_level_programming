1ad98eeb025f841586538a1d4d474755613493b7
ANAGONOU Armand <info.armanagonou.com>
2022-10-31 11:23:57 +0100

    init

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * string_nconcat
+ *
+ * @s1: str1
+ * @s2: str2
+ * @n: num of bites
+ *
+ * Return: 
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
