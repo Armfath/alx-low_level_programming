c44df535adc8ba99482a143438915f2e31572ae2
Armfath <info.armanangou@gmail.com>
2022-10-31 16:43:02 +0100

    test read and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: the filename
+ * @letters: the number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t n;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters + 1);
+	n = read(fd, buf, letters);
+	buf[letters + 1] = '\0';
+	printf("%s", buf);
+
+	return (n);
+}
