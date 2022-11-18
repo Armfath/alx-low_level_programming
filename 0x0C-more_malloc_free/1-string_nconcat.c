56f84b2021954cad67fb3565895faf88f47bdad6
ANAGONOU Armand <info.armanagonou.com>
2022-11-18 13:15:41 +0100

    str_n_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,5 +10,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len1 = 0, len2 = 0, len = 0, i, j = 0;
+	char *s;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (len2 < n)
+	{
+		n = len2;
+	}
+	len = len1 + n + 1;
+	s = malloc(sizeof(char) * len);
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (; i < len; i++)
+	{
+		s[i] = s2[j];
+		j++;
+	}
+
+	return (s);
 }
