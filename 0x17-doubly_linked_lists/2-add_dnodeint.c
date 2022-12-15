cf8eaf098d75d806c1a394aacc2d58cd981b420e
ANAGONOU Armand <info.armanagonou.com>
2022-12-15 10:44:08 +0100

    add node at the begining

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,15 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	if (head == NULL)
-	{
-		new->next = NULL;
-		new->prev = NULL;
-		(*head) = new;
-		return (*head);
-	}
-	new->next = (*head);
 	new->prev = NULL;
+	new->next = (*head);
+	(*head)->prev = new;
 	(*head) = new;
 
 	return (*head);
