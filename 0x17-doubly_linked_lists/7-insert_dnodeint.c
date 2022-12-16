1e2c77c6cff66173e400be66b7e1d102061c7083
ANAGONOU Armand <info.armanagonou.com>
2022-12-16 20:37:23 +0100

    test add node anywhere

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *new;
 	int i = 0;
 
-	if ((*h) == NULL)
-	{
-		return (NULL);
-	}
+	
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
