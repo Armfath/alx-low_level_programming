366f1221333840dcea4225af580b7589bd7c7a52
ANAGONOU Armand <info.armanagonou.com>
2022-12-16 19:00:42 +0100

    test add node anywhere

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,10 +37,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (temp)
 			i++;
 	}
-	if ((int)idx == i)
+	if ((int)idx == i + 1)
 	{
 		add_dnodeint_end(h, n);
 		return (new);
 	}
+
 	return (NULL);
 }
