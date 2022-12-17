e91680b39da143b5de5abce885f2beca988446bb
ANAGONOU Armand <info.armanagonou.com>
2022-12-17 16:00:40 +0100

    delete a node

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *next, *curr;
+	int i = -1;
+
+	temp = (*head);
+	/* printf("temp->n = %d", temp->n); */
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (((int)index < 0 || (int)index > i))
+		return (-1);
+	if (index == 0 && (*head)->next == NULL)
+	{
+		free(*head);
+		*head = NULL;
+		return (1);
+	}
+	if (index == 0 && (*head)->next != NULL)
+	{
+		(*head) = (*head)->next;
+		(*head)->prev = NULL;
+		return (1);
+	}
+	if ((int)index == i)
+	{
+		temp = (*head);
+		for (; index > 0; index--)
+			temp = temp->next;
+		temp->next = NULL;
+		return (1);
+	}
+	temp = (*head);
+	for (; index > 1; index--)
+		temp = temp->next;
+	curr = temp->next;
+	next = curr->next;
+	temp->next = next;
+	next->prev = temp;
+	return (1);
+}
