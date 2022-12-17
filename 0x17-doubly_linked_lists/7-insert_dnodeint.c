48996170c4bb9b92a5fb67f6de5a2053f6b386ed
ANAGONOU Armand <info.armanagonou.com>
2022-12-17 05:25:39 +0100

    add node anywhere

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,10 +9,17 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *new;
-	int i = 0;
+	dlistint_t *temp, *new, *next;
+	int i = -1;
 
-	
+	temp = (*h);
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if ((int)idx > i + 1 || (int)idx < 0)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
@@ -22,28 +29,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (*h == NULL)
-	{
-		*h = new;
-		return (new);
-	}
-	if (idx == 0)
+	if (*h == NULL || idx == 0)
 	{
 		add_dnodeint(h, n);
 		return (new);
-	}
-	temp = (*h);
-	while (temp)
-	{
-		temp = temp->next;
-		if (temp)
-			i++;
 	}
 	if ((int)idx == i + 1)
 	{
 		add_dnodeint_end(h, n);
 		return (new);
 	}
+	temp = (*h);
+	for (; idx > 1; idx--)
+		temp = temp->next;
+	next = temp->next;
+	new->prev = temp;
+	temp->next = new;
+	new->next = next;
+	next->prev = new;
 
-	return (NULL);
+	return (new);
 }
