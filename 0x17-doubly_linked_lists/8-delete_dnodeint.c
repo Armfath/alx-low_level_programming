c8f50afb9bfebcd0352fdb0ed7f3ac567806c1cf
ANAGONOU Armand <info.armanagonou.com>
2022-12-18 08:25:55 +0100

    delete node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete node
+ *
+ * @head: head
+ * @index: index
+ * Return: the list
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp, *next, *curr;
