f378fc3bcfae33636bce5c50c4ae8c1464b3dfb9
ANAGONOU Armand <info.armanagonou.com>
2023-04-12 12:17:09 +0100

    Linear Algo for search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,12 +12,14 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+	char msg[50];
 
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		sprintf(msg, "Value checked array[%ld] = [%d]\n", i, array[i]);
+        write(STDOUT_FILENO, msg, strlen(msg));
 		if (array[i] == value)
 			return (i);
 	}
